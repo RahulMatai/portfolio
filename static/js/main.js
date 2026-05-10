5d2632947538b36b5f05bd7e1e0bd1a3a1de00bf
Rahulmatai <rahulmatai73@gmail.com>
2026-05-10 14:41:03 +0530

    fix: valid HuggingFace color config

--- static/js/main.js
+++ static/js/main.js
@@ -142,7 +142,7 @@ async function loadProjects() {
 // Fetch and render blog posts dynamically
 async function loadBlogs() {
     try {
-        const response = await fetch('/Blogs');
+        const response = await fetch('/blogs');
         const data = await response.json();
         
         const grid = document.getElementById('blogGrid');
